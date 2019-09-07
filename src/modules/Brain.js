3f099a90c968d667c39546c7c50401817d7572e1
unknown <okumura6883@gmail.com>
2019-09-07 05:07:06 -0700

    I HAVE GIGANTIC BRAIN

--- src/modules/Brain.js
+++ src/modules/Brain.js
@@ -1,4 +1,8 @@
-const nsfwjs = require("nsfwjs");
+const tf = require("@tensorflow/tfjs-node");
+const { Canvas, loadImage } = require("canvas");
+const express = require("express");
+
+const NSFW_CLASSES = ['Drawing', 'Hentai', 'Neutral', 'Porn', 'Sexy'];
 
 class Brain {
 
@@ -7,7 +11,56 @@ class Brain {
      */
     constructor(app) {
         this.app = app;
-        console.log(nsfwjs);
+        // this.selfPort = 3001;
+        this.ready = false;
+        this.size = 299;
+        this.canvas = new Canvas(299, 299);
+        this.ctx = this.canvas.getContext("2d");
+        this.init();
+    }
+
+    async init() {
+        let logger = this.app.logger;
+
+        let now = Date.now();
+        logger.inform("Loading NSFW model");
+        this.model = await tf.loadLayersModel(tf.io.fileSystem(__dirname + "/../../nsfw_model/model.json"));
+        logger.inform(`NSFW model loaded, time elasped: ${Date.now() - now}ms`);
+
+        // /** @type {tf.Tensor<tf.Rank>} */
+        // let testResult = await tf.tidy(() => this.model.predict(tf.zeros([1, this.size, this.size, 3])));
+
+        // let resultArray = testResult.arraySync()[0];
+        
+        // let result = resultArray.reduce((prev, curr, index) =>{
+        //     prev[NSFW_CLASSES[index]] = curr;
+        //     return prev;
+        // }, {});
+    }
+
+    /** @param {string} src */
+    async classify(src) {
+        let img = await loadImage(src);
+
+        this.ctx.arc(this.size / 2, this.size / 2, this.size / 2, 0, Math.PI * 2);
+        this.ctx.clip();
+        this.ctx.drawImage(img, 0, 0, this.size, this.size);
+
+        let input = tf.browser
+            .fromPixels(this.canvas)
+            .toFloat()
+            .div(tf.scalar(255));
+
+        let testResult = await tf.tidy(() => this.model.predict(tf.expandDims(input, 0)));
+
+        let resultArray = testResult.arraySync()[0];
+        
+        let result = resultArray.reduce((prev, curr, index) =>{
+            prev[NSFW_CLASSES[index]] = curr;
+            return prev;
+        }, {});
+
+        return result;
     }
 
 }

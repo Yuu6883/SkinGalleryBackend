9f23467ab92a1fce635391cbbed22640ac25d4b7
unknown <okumura6883@gmail.com>
2019-10-29 10:45:37 -0700

    Added eval to fix shit on the fly

--- src/modules/DiscordBot.js
+++ src/modules/DiscordBot.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const fetch = require("node-fetch");
+const { inspect } = require("util");
 const DiscordJS = require("discord.js");
 const { execSync } = require("child_process");
 const { Attachment, RichEmbed, Client } = DiscordJS;
@@ -149,6 +149,47 @@ class VanisSkinsDiscordBot extends Client {
             let userID = message.content.replace(/\D/g, "").trim();
             this.unban(userID, message);
         }
+
+        if (message.content.startsWith(`${this.prefix}eval `)) {
+            this.runEval(message);
+        }
+    }
+
+    /** 
+     * @param {DiscordJS.Message} message 
+     */
+    async runEval(message) {
+        let code = message.content.replace(`${this.prefix}eval `, "");
+        const start = process.hrtime.bigint();
+        let channel = message.channel;
+        let guild = message.guild;
+
+        try {
+            let evaled = eval(code);
+            if (evaled instanceof Promise) evaled = await evaled;
+            const end = process.hrtime.bigint();
+            const type = evaled && typeof evaled === 'object' && evaled.constructor ? evaled.constructor.name : typeof evaled;
+            const output = inspect(evaled, {
+                depth: 0,
+                maxArrayLength: 100
+            }).replace(new RegExp(this.config.discordBotToken, "g"), "secret bruh");
+    
+            message.reply(new RichEmbed()
+                .setDescription(`**📥 Input**\n\`\`\`js\n${code}\n\`\`\`\n**📤 Output**\n\`\`\`js\n${output}\n\`\`\`\n**❔ Type:** \`${type}\``)
+                .setFooter(`executed in ${Number(end - start) / 1000000} milliseconds`, message.author.displayAvatarURL)
+            );
+        } catch (error) {
+			const end = process.hrtime.bigint();
+			error = inspect(error, {
+				depth: 0,
+				maxArrayLength: 0
+            });
+            
+            message.reply(new RichEmbed()
+                .setDescription( `**📥 Input**\n\`\`\`js\n${code}\n\`\`\`\n**❗ Error:**\n\`\`\`\n${error}\n\`\`\``)
+                .setFooter(`executed in ${Number(end - start) / 1000000} milliseconds`, message.author.displayAvatarURL)
+            );
+		}
     }
 
     /**

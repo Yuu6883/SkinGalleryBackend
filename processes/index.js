70fc80b7af0a607c84054a9393e9f8cc944164c1
unknown <okumura6883@gmail.com>
2019-11-02 14:42:40 -0700

    Passing all env to pm2 processes

--- processes/index.js
+++ processes/index.js
@@ -7,7 +7,7 @@ pm2.connect(async err => {
     await new Promise(resolve => pm2.start({
         script: `${__dirname}/bot.js`,
         name: "BOT",
-        env: { NODE_ENV: process.env.NODE_ENV },
+        env: process.env,
         restart_delay: 10000,
         max_memory_restart: "75M"
     }, err => {
@@ -19,7 +19,7 @@ pm2.connect(async err => {
     await new Promise(resolve => pm2.start({
         script: `${__dirname}/nsfw.js`,
         name: "NSFW",
-        env: { NODE_ENV: process.env.NODE_ENV },
+        env: process.env,
         restart_delay: 10000,
         max_memory_restart: "400M"
     }, err => {
@@ -32,7 +32,7 @@ pm2.connect(async err => {
         script: `${__dirname}/server.js`,
         name: "SERVER",
         restart_delay: 10000,
-        env: { NODE_ENV: process.env.NODE_ENV },
+        env: process.env,
         exec_mode: "cluster",
         instances: Math.min((require("os").cpus().length - 1) || 1, 4),
         max_memory_restart: "65M"

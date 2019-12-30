7d359b11286a2c3a699f21b8fbd3bd6a47844c02
Yuu <pog@yuu.studio>
2019-12-30 02:29:02 -0800

    Allowing moderator to upload multiple skins at once (abuse)

--- src/api/loginManually.js
+++ src/api/loginManually.js
@@ -5,12 +5,9 @@ const endpoint = {
     handler(req, res) {
         if (!hasPermission("LOGIN", req.vanisPermissions))
             return void res.sendStatus(403);
-        // Lmao
-        let redir = process.platform == "win32" ? 
-                "http://localhost/api/login/callback" : this.config.discordAppRedirect;
-
+        
         res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${this.config.discordAppID}` + 
-                `&scope=identify&response_type=code&redirect_uri=${redir}`);
+                `&scope=identify&response_type=code&redirect_uri=${this.config.discordAppRedirect}`);
     },
     method: "get",
     path: "/login"
